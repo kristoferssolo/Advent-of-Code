59e09a0eb7747d08b73d92f0e5c3a60c98fece1c
Kristofers Solo <kristians.cagulis@pm.me>
2023-12-04 16:14:29 +0200

    fix daily-template

--- 2023/daily-template/src/part2.rs
+++ 2023/daily-template/src/part2.rs
@@ -1,7 +1,8 @@
 use color_eyre::Result;
 
-pub fn process(input: &str) -> Result<()> {
+pub fn process(input: &str) -> Result<u32> {
     todo!("day xx - part 2");
+    Ok(0)
 }
 
 #[cfg(test)]
@@ -12,7 +13,7 @@ mod tests {
     fn test_process() -> Result<()> {
         let input = "";
         todo!("haven't built test yet");
-        assert_eq!("", process(input)?);
+        assert_eq!(0, process(input)?);
         Ok(())
     }
 }
