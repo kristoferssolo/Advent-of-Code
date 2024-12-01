daf34520d6595c37cb0c5a28951c2a5e55de906c
Kristofers Solo <dev@kristofers.xyz>
2024-12-01 17:26:06 +0200

    day-01 part-1

---
+++ 2024/day-01/src/part1.rs
@@ -0,0 +1,45 @@
+use color_eyre::Result;
+
+pub fn process(input: &str) -> Result<usize> {
+    let (mut firsts, mut lasts): (Vec<_>, Vec<_>) = input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let nums = line
+                .split_whitespace()
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            (*nums.first().unwrap(), *nums.last().unwrap())
+        })
+        .unzip();
+
+    firsts.sort();
+    lasts.sort();
+
+    let sum = firsts
+        .iter()
+        .zip(lasts.iter())
+        .map(|(first, last)| first.abs_diff(*last))
+        .sum();
+    Ok(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> Result<()> {
+        let input = "
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+        let result = 11;
+        assert_eq!(process(input)?, result);
+        Ok(())
+    }
+}
