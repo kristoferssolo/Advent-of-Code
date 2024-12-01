93a3c0a932419d242677f15a0acda55bf522d579
Kristofers Solo <dev@kristofers.xyz>
2024-12-01 16:31:58 +0200

    2024

---
+++ 2024/daily-template/src/part2.rs
@@ -0,0 +1,20 @@
+use color_eyre::Result;
+
+pub fn process(input: &str) -> Result<usize> {
+    todo!("day xx - part 2");
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> Result<()> {
+        let input = "";
+        todo!("haven't built test yet");
+        let result = 0;
+        assert_eq!(process(input)?, result);
+        Ok(())
+    }
+}
