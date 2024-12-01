3c899e77824d0da58dcc0364d9f620af12fcd376
Kristofers Solo <dev@kristofers.xyz>
2024-12-01 17:38:42 +0200

    day-01 part-2

--- 2024/day-01/src/part2.rs
+++ 2024/day-01/src/part2.rs
@@ -1,8 +1,28 @@
 use color_eyre::Result;
 
 pub fn process(input: &str) -> Result<usize> {
-    todo!("day xx - part 2");
-    Ok(0)
+    let sum = {
+        let (firsts, lasts): (Vec<_>, Vec<_>) = input
+            .lines()
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                let nums = line
+                    .split_whitespace()
+                    .map(|num| num.parse::<usize>().unwrap())
+                    .collect::<Vec<_>>();
+                (*nums.first().unwrap(), *nums.last().unwrap())
+            })
+            .unzip();
+
+        firsts
+            .iter()
+            .map(|x| {
+                let count = lasts.iter().filter(|&y| y == x).count();
+                x * count
+            })
+            .sum()
+    };
+    Ok(sum)
 }
 
 #[cfg(test)]
@@ -11,9 +31,16 @@ mod tests {
 
     #[test]
     fn test_process() -> Result<()> {
-        let input = "";
-        todo!("haven't built test yet");
-        assert_eq!(0, process(input)?);
+        let input = "
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+        let result = 31;
+        assert_eq!(process(input)?, result);
         Ok(())
     }
 }
