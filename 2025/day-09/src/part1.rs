838f24ea5dcf24a55aa8eee71d6ddf80e6db2282
Kristofers Solo <dev@kristofers.xyz>
2025-12-09 08:11:32 +0200

    Init day-09

---
+++ 2025/day-09/src/part1.rs
@@ -0,0 +1,21 @@
+#[tracing::instrument]
+#[allow(clippy::missing_panics_doc)]
+#[allow(clippy::missing_errors_doc)]
+pub fn process(input: &str) -> miette::Result<usize> {
+    todo!("day xx - part 1");
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "";
+        todo!("haven't built test yet");
+        let result = 0;
+        assert_eq!(process(input)?, result);
+        Ok(())
+    }
+}
